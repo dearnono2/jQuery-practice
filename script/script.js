fbdb2b48abaec3d854c1ae712117cc0691203c0a
dearnono2 <dearnono2@gmail.com>
2023-01-22 00:08:16 +0900

    first commit

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ $(function() {
   let boxes = $('.box');
   $('.container').on('click', function() {
     $(this).css('background-color', 'green'),
-    $(this).text('저장된 box의 총 갯수는' + boxes.length + '개 입니다.'),
+    $(this).text('저장된 box의 총 갯수는' + boxes.length + '개 입니다~'),
     $('#lens').css('color', 'blue');
   })
 })
