0905dc12d7de81dac58db2a1d276beddc18bea8d
dearnono2 <dearnono2@gmail.com>
2023-01-26 23:34:03 +0900

    방법2와 주석 추가

--- 04.list_toggle/script.js
+++ 04.list_toggle/script.js
@@ -12,10 +12,30 @@ lists.click(function() {
   }
 })
 
-// 두가지 방법 모두 작동하지만 위의 코드가 더 효율적이다.
-
 
 // 방법 2
+// 자바스크립트의 숫자는 무한대가 아니므로 숫자의 끝에 도달하면 에러가 생길 수 있다!
+// 그렇기 때문에 방법 2와같이 숫자를 계속해서 더하는 로직을 사용하기 위해서는 숫자를 담은 변수(ex: 아래의 num)를 초기화 해주는것이 좋다!
+
+// +방법 2는 오류가 있다! 만약 리스트1을 클릭하고 곧바로 리스트2를 클릭하게 된다면 바로 열리지 않는다... 리스트 2를 선택할 때 num은 짝수이기 때문에 클래스명 on을 삭제하기 때문이다!
+// let num = 0;
+
+// $('li').click(function() {
+//   num += 1
+//   console.log(num);
+//   if(num % 2 !== 0) {
+//     $(this).addClass('on');
+//     $(this).find('.text').text('close');
+//   } else {
+//     $(this).removeClass('on');
+//     $(this).find('.text').text('open');
+//     num = 0;
+//     // 값 초기화 해주기
+//   }
+// })
+
+
+// 방법 3
 // lists.each(function (index, element) {
 //   $(element).click(function () {
 //     if ($(element).hasClass("on")) {
