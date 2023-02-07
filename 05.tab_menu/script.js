70f02f79f02290476ba2f393de26d8509378fd00
dearnono2 <dearnono2@gmail.com>
2023-02-07 22:50:42 +0900

    jQuery ui 라이브러리로 tab menu 구현(css 확인 필요)

--- 05.tab_menu/script.js
+++ 05.tab_menu/script.js
@@ -1,21 +1,7 @@
 $(function() {
-  let tabAnchor = $('.tabs-nav li a');
-  let tabPanel = $('.tabs-panel');
-
-  // 링크를 클릭하면 할 일
-  tabAnchor.click(function(e) {
-    e.preventDefault();
-
-    $(this).find('a').addClass('active'); // 내가 클릭한 a태그
-    $(this).siblings().find('a').removeClass('active'); // 내가 클릭한 것 외의 나머지들 안의 a태그를 제거
-    
-    tabPanel.hide();
-
-    let $targetIdx = $(this).index();
-    tabPanel.eq($targetIdx).show();
-
-  })
-
-  // .trigger 강제로 클릭 이벤트 발생시키기
-  tabAnchor.eq(0).trigger('click');
+  // jQuery ui 라이브러리 사용.
+  $('.work-section').tabs({
+    show: { duration: 400 },
+    hide: { duration: 400 } 
+  });
 })
