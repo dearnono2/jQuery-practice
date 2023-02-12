b40f3944802897c890bd6bdc594de0328e3ac616
dearnono2 <dearnono2@gmail.com>
2023-02-12 21:11:34 +0900

    animateNumber %나오게 하기 + fin

--- 08.number_animation_by_scroll_2/script.js
+++ 08.number_animation_by_scroll_2/script.js
@@ -1,7 +1,29 @@
 
 // 이 선택자의 요소가 화면에 나타날 때에 할 일
-var waypoints = $('#options-only').waypoint({
-  handler: function(direction) {
-    notify(this.element.id + ' hit')
+// var waypoints = $('#options-only').waypoint({
+//   handler: function(direction) {
+//     notify(this.element.id + ' hit')
+//   }
+// })
+
+$(function() {
+  let executed = false;
+  $('.section2').waypoint(function() {
+    let targetNumber = $('.animate').attr('data-rate');
+    if(!executed) {
+      // %도 나오게 하기
+      var percent_number_step = $.animateNumber.numberStepFactories.append(' %')
+      $('.animate').animateNumber(
+        { 
+        number: targetNumber,
+        numberStep: percent_number_step,
+        },
+        1500);
+      executed = true;
+    }
+  },
+  {
+    offset: '80%'
   }
+  )
 })
